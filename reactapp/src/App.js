8cf801612ddb5d0f3d63fafd39fe6ace1d83f8fd
Pratik Mandge <88229260+pratikmandge@users.noreply.github.com>
2023-03-12 21:43:42 +0530

    Add files via upload
    Uploaded project

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import Stopwatch from './components/Stopwatch/Stopwatch.jsx';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <Stopwatch />
+      </div>
+    </div>
+  );
+}
+
+export default App;
